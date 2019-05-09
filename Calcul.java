b67a92e5a007a475a46ce3d681f6bc94e8564abc
yonelT <49525545+yonelT@users.noreply.github.com>
2019-05-10 00:10:24 +0200

    Le nom de la classe n'avait pas changer: la premiere lettre n'était pas en majuscule

---
+++ Calcul.java
@@ -0,0 +1,28 @@
+package ex1;
+
+/**
+ * @author Diginamic
+ * 
+ */
+
+public class Calcul {
+
+	/** Sert a faire des additions
+	 * 
+	 * @param int a: premier numero
+	 * @param int b: deuxieme numero
+	 * @return int : resultat de l'addition
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+	
+	/** Sert a faire des soustractions
+	 * @param int a: premier numero
+	 * @param int b: deuxieme numero
+	 * @return int: resultat de la soustraction
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
